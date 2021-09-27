e4b20c2b4a0f00fb6d8de386ce4b2adb79e45958
unknown <jvas1999@gmail.com>
2021-09-27 19:01:02 -0300

    up das atv

---
+++ atv2.js
@@ -0,0 +1,69 @@
+//A
+let x=5;
+let y=8;
+
+var resultado = x===y
+
+console.log(validacao);
+
+//B
+let x2=6
+let y2="6"
+
+var resultado1 = x2===y2
+
+console.log(validacao1);
+
+
+//C
+let x3="7";
+let y3=7;
+
+let resultado3 = x3==y3
+
+console.log(resultado3);
+
+//2
+let nome = "fulano";
+
+console.log("Boa sorte, "+nome);
+
+//3
+let x4=10
+let y4=5
+
+console.log("x = "+(x4+=y4));
+
+//b
+let x4=10
+let y4=5
+
+console.log("x = "+(x4*=y4));
+
+//c
+let x4=10
+let y4=5
+
+console.log("x = "+(x4=0));
+
+//D
+let x4=10
+let y4=5
+
+console.log("x = "+(x4/=y4));
+
+//E
+let x4=10
+let y4=5
+
+console.log("x = "+(x4=y4));
+
+
+
+
+
+
+
+
+
+
---
+++ atv3.js
@@ -0,0 +1,21 @@
+//1
+var numero = 25;
+
+console.log(numero%3);
+
+//2
+let texto1 = "Programa Start - "
+let texto2 = "formação Front end: "
+let texto3 = "eu faço parte dessa oportunidade."
+console.log(texto1 + texto2 + texto3)
+
+//3)
+let texto1 = "Mariazinha tem ";
+let idade =  9;
+let texto2 = " anos e ";
+let quantidade_irmaos = 7;
+let texto3 = " irmãos.";
+console.log(texto1 + idade + texto2 + quantidade_irmaos + texto3)
+
+
+
